41ed6d3fef31fa3bf7c1afbd48555b2c2d3d582c
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2022-03-19 11:56:42 +0500

    Full Stack done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-
-import './App.css';
-import Home from './pages/Home/Home';
-import Profile from './pages/Profile/Profile';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Register from './pages/register/Register';
-import Login from './pages/login/Login';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={< />} /> */}
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-     
-      </Routes>
-      <div> Foooter </div>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
     </Router>
   );
 }
